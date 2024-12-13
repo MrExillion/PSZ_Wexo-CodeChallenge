3125e707e7e99d388c4b100ee2d841b5e45596ca
Peder Sloth Züricho <peder@zuricho.dk>
2024-12-13 05:00:07 +0100

    some clean up, some start to mixin in this process as well. Updated the first index file to use some php as well.

---
+++ EasyFlix/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE php>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <?php include("./EasyFlix_Top.html"); ?>
+  <body>
+    <?php include("./EasyFlix_Templates.html");
+    include("./EasyFlix_Main.html"); ?>
+  </body>
+  <?php include("./EasyFlix_Footer.html");?>
+</html>
